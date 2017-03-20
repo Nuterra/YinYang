b83505757696704cbd4ae2169b5b7a1c7a130532
maritaria <bramkamies@gmail.com>
2017-03-20 16:32:55 +0100

    Fixed async loop
    Note: when using modern browsers, they may attempt to reuse connections
    such that long blocking operations will have to wait on eachother

--- YinYang/Program.cs
+++ YinYang/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace YinYang
 {
@@ -6,13 +7,21 @@ namespace YinYang
 	{
 		private static void Main(string[] args)
 		{
-			Server s = new Server("http://localhost/");
+			Server s = new Server();
+			s.Listener.Prefixes.Add("http://localhost/");
+			s.Listener.Prefixes.Add("http://127.0.0.1/");
+
 			s.AddRoute(new HttpRoute("/api/mydata", HttpMethod.Get, HttpMethod.Post), new MyDataHandler());
 			s.AddRoute(new HttpRoute("/wait", HttpMethod.Get), new WaitHandler());
 			s.AddRoute(new HttpRoute("/", HttpMethod.Get), new StaticFileHandler());
-			s.Start();
-			Console.WriteLine("Server Started");
+
+			CancellationTokenSource tokenSource = new CancellationTokenSource();
+			CancellationToken token = tokenSource.Token;
+			s.Listener.Start();
+			s.RunListenLoop(token);
+			Console.WriteLine("Server Started, press enter to shutdown");
 			Console.ReadLine();
+			tokenSource.Cancel();
 			s.Stop();
 		}
 	}
--- YinYang/Server.cs
+++ YinYang/Server.cs
@@ -8,19 +8,8 @@ namespace YinYang
 {
 	public sealed class Server
 	{
-		private HttpListener _listener = new HttpListener();
+		public HttpListener Listener { get; } = new HttpListener();
 		private Dictionary<HttpRoute, RequestHandler> _routing = new Dictionary<HttpRoute, RequestHandler>();
-		public int MaxConcurrentRequests { get; set; } = 10;
-		public string BaseUrl { get; }
-
-		public Server(string baseUrl)
-		{
-			if (!HttpListener.IsSupported)
-			{
-				throw new NotSupportedException("HttpListener is not supported");
-			}
-			BaseUrl = baseUrl;
-		}
 
 		public void AddRoute(HttpRoute route, RequestHandler handler)
 		{
@@ -30,49 +19,20 @@ namespace YinYang
 			_routing.Add(route, handler);
 		}
 
-		public void Start()
+		public async Task RunListenLoop(CancellationToken token)
 		{
-			if (_listener.IsListening) throw new InvalidOperationException("Server already started");
-			_listener.Start();
-			_listener.Prefixes.Add(BaseUrl);
-
-			var c = TaskScheduler.Current;
-
-			var l = Listen();
-		}
-
-		private async Task Listen()
-		{
-			var taskPool = new HashSet<Task>();
-			for (int i = 0; i < MaxConcurrentRequests; i++)
-				taskPool.Add(_listener.GetContextAsync());
-
-			while (_listener.IsListening)
+			//_listener.BeginGetContext(ClientAvailable, null);
+			while (!token.IsCancellationRequested)
 			{
-				Console.WriteLine($"Waiting... {Thread.CurrentThread.ManagedThreadId}");
-				Task task = await Task.WhenAny(taskPool);
-				taskPool.Remove(task);
-
-				Console.WriteLine($"Task completed {Thread.CurrentThread.ManagedThreadId}");
-
-				if (task is Task<HttpListenerContext>)
-				{
-					var context = (task as Task<HttpListenerContext>).Result;
-					taskPool.Add(_listener.GetContextAsync());
-					taskPool.Add(Task.Factory.StartNew(HandleClient, context));
-					//taskPool.Add(HandleClient(context));
-				}
+				var context = await Listener.GetContextAsync();
+				HandleClient(context);
 			}
 		}
 
-		private async Task HandleClient(object c)
+		private async Task HandleClient(HttpListenerContext context)
 		{
-			HttpListenerContext context = (HttpListenerContext)c;
 			try
 			{
-				Console.WriteLine($"Begin handle {context.GetHashCode()} {Thread.CurrentThread.ManagedThreadId}");
-				Console.WriteLine($"Request: {context.Request.RawUrl}");
-
 				foreach (KeyValuePair<HttpRoute, RequestHandler> route in _routing)
 				{
 					if (route.Key.CanAccept(context.Request))
@@ -84,14 +44,13 @@ namespace YinYang
 			}
 			finally
 			{
-				Console.WriteLine($"End handle {context.GetHashCode()} {Thread.CurrentThread.ManagedThreadId}");
 				context.Response.OutputStream.Close();
 			}
 		}
 
 		public void Stop()
 		{
-			_listener.Stop();
+			Listener.Stop();
 		}
 	}
 }
