199abc3a8e10451c9d1b1c6052a113530dfb5ebc
maritaria <bramkamies@gmail.com>
2017-04-05 21:56:28 +0200

    Added downloads page
    Also merged /style with /pages

---
+++ YinYang/assets/pages/download.js
@@ -0,0 +1,11 @@
+﻿var downloadTemplate;
+Nuterra.loadTemplate('tmpl-download', '/assets/pages/download.html', function (template) {
+    downloadTemplate = template;
+    Mustache.parse(downloadTemplate);
+});
+
+Nuterra.addPage('download', function (id) {
+    var rendered = Mustache.render(downloadTemplate, {});
+    $('#main-content').html(rendered);
+    $('#main-content .btn').button();
+});
