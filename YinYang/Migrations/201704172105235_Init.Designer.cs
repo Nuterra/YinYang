5581f0c211ecb2f217b4eb2370f61058a405e0b4
maritaria <bramkamies@gmail.com>
2017-04-17 23:44:03 +0200

    Rebuild migrations for production

--- YinYang/Migrations/201704172105235_Init.Designer.cs
+++ YinYang/Migrations/201704172105235_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace YinYang.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201704171245540_init"; }
+            get { return "201704172105235_Init"; }
         }
         
         string IMigrationMetadata.Source
